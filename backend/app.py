cee3883fb553428db284e596fe23f08758565934
Gabriel Hernandez <gabrielhernandez202333@gmail.com>
2024-03-16 10:20:17 -0400

    Implementadas sesiones
    Se han implementado las sesiones en el backend junto con un sistema para
    llevar la cuenta de la cantidad de mensajes hechas por un usuario
    anonimo a la IA. Tambien se incremento el limite de caracteres por
    mensaje de 50 a 100

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_session import Session
 
 from controllers import register_endpoints
 
+SESSION = Session()
+
 def create_app():
     app = Flask(__name__)
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SESSION_FILE_DIR'] = 'sessions'
+
+    SESSION.init_app(app)
 
     register_endpoints(app)
 
--- backend/controllers/prompts.py
+++ backend/controllers/prompts.py
@@ -1,17 +1,17 @@
 from os import getenv
 
 import google.generativeai as genai
-from flask import Response, jsonify, request
+from flask import Response, jsonify, request, session
 from flask.blueprints import Blueprint
 
 prompts_bp = Blueprint('prompts', __name__)
 
 GOOGLE_API_KEY = getenv('GOOGLE_API_KEY') # TODO mover al módulo app
 if not GOOGLE_API_KEY:
-    raise Exception('text') # TODO cambiar el tipo de excepción y su texto
+    raise ValueError('No Google API key provided')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-@prompts_bp.route('/getanswer', methods=['GET'])
+@prompts_bp.route('/answer', methods=['GET'])
 def get_ai_answer():
     """Envía un prompt a GPT-3.5 con la personalidad dada para obtener una respuesta.
 
@@ -29,16 +29,26 @@ def get_ai_answer():
                 Content:
                 - message: La respuesta provista por GPT-3.5.
     """
+    if not session.get('username'):
+        return Response('Anonymous user has not been set', status=403)
+
+    if not request.json:
+        return Response('Request must have JSON body', status=400)
+
     promt = request.json.get('prompt')
     if not promt:
         return Response('You have to provide a prompt', status=400)
-    elif len(promt) > 50:
+    elif len(promt) > 100:
         return Response('The prompt is too long', status=400)
 
     model = genai.GenerativeModel('gemini-pro')
 
     # TODO usar start_chat junto con sesiones para mantener una conversación
-    # TODO limitar el número de mensajes a unos 50 (y limitar el número de caracteres también)
     response = model.generate_content(promt) # TODO verificar cuando el modelo da errores por uso inadecuado
 
-    return jsonify(msg=response.text)
+    if session['message_count'] == 50:
+        return Response('Message limit exceeded', status=403) # TODO verificar si el status code usado es el adecuado
+
+    session['message_count'] += 1
+
+    return jsonify(msg=response.text, msg_count=session['message_count'])
