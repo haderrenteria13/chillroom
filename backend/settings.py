53824893adf08868443ec39d0f5a384bbb6ed32d
Gabriel Hernandez <gabrielhernandez202333@gmail.com>
2024-03-16 13:01:19 -0400

    Creado modulo settings
    Este modulo va a contener diferentes configuraciones para el backend que
    no tienen porque estar en variables de entorno. Por ahora solo contiene
    configuraciones de seguridad para las respuestas del modelo de IA

---
+++ backend/settings.py
@@ -0,0 +1,8 @@
+from google.generativeai.types import HarmBlockThreshold, HarmCategory
+
+SAFETY_SETTINGS = {
+    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH
+}
