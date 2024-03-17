4e54e03a65965750db366b8267ea5bb96528c667
Hader <haderrenteria13az@gmail.com>
2024-03-16 19:11:19 -0500

    Creacion de Registro, Inicia sesión y bienvenida

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-export const Welcome = () => {
+const Welcome = () => {
   return (
-    <div>Welcome</div>
-  )
-}
+    <div className="bg-[url('./src/assets/landing-page.png')] bg-cover bg-center h-screen">
+      <div className='container mx-auto'>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-white text-[80px] font-bold">Bienvenido a</h1>
+          <h2 className="text-[#7EBAFB] text-[60px] font-bold">ChillRoom</h2>
+          <p className="text-slate-400 text-sm">La transmisión del conocimiento se transforma en una experiencia apasionante.</p>
+
+          <div className='pt-8'>
+            <a className="border-[3px] border-white bg-white/5 backdrop-blur-sm text-sm rounded-full font-medium text-white px-12 py-2 hover:bg-white hover:text-black transition-colors flex items-center" href="#">
+              Explora, aprende y conecta  
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
