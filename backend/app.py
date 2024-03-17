5e9925a3e0251829409b22390dde0ddae851c129
GabrielEHT <gabrielhernandez202333@gmail.com>
2024-03-17 15:12:11 -0400

    Cambios para el deploy
    Se han hecho unos cambios relacionados con la
    carga de archivos para que sea funcional en el
    entorno de producción

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+from os import getcwd, getenv
+from os.path import join
+
 from dotenv import load_dotenv
 from flask import Flask
 from flask_session import Session
@@ -7,7 +10,11 @@ from controllers import register_endpoints
 SESSION = Session()
 
 def create_app():
-    load_dotenv()
+    if getenv('FLASK_DEBUG'):
+        load_dotenv()
+    else:
+        load_dotenv(join(getcwd(), 'chillroom/backend/.env'))
+
     app = Flask(__name__)
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['SESSION_FILE_DIR'] = 'sessions'
