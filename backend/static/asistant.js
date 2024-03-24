103d718eae4322e798b4c82a397b0788ce9b90f2
Isa696 <isaias92@hotmail.com.ar>
2024-03-24 13:02:18 -0300

    arreglado variables has already been declared

--- backend/static/asistant.js
+++ backend/static/asistant.js
@@ -1,13 +1,15 @@
-document.getElementById("title-user").innerText = sessionStorage.getItem("userName");
+document.getElementById("title-user").innerText = sessionStorage.getItem('userName');
 
-// Función para manejar los eventos de clic y almacenar el ID en sessionStorage
 function handleAsistantClick(event) {
     const clickedIAText = event.target.innerText;
     const clickedIAid = event.target.id;
-    sessionStorage.setItem('asistantSelected', clickedIAText);
-    sessionStorage.setItem('asistantSelectedId', clickedIAid);
-    console.log(`ID clickeado: ${clickedIAid} almacenado en sessionStorage.`);
-    console.log(`ID clickeado: ${clickedIAText} almacenado en sessionStorage.`);
+
+    sessionStorage.setItem('assistantSelected', clickedIAText);
+
+    sessionStorage.setItem('assistantSelectedId', clickedIAid);
+
+    console.log(`ID clickeado: ${clickedIAid}`);
+    console.log(`ID clickeado: ${clickedIAText}`);
 }
 /* //animacion fadeout
 let animateFadeout = document.getElementsByClassName("fadeout");
@@ -23,13 +25,3 @@ document.getElementById('mentor').addEventListener('click', handleAsistantClick 
 document.getElementById('arty').addEventListener('click', handleAsistantClick );
 document.getElementById('sunny').addEventListener('click', handleAsistantClick );
 document.getElementById('spark').addEventListener('click', handleAsistantClick );
-
-let assistantSelected = sessionStorage.getItem('asistantSelected');
-let chatIaName = document.getElementsByClassName('chatIa');
-
-let assistantSelectedId = sessionStorage.getItem('asistantSelectedId');
-let chatIaContainer = document.getElementsByClassName('chat-ia-container');
-
-
-let userName = sessionStorage.getItem('userName');
-let chatUserName = document.getElementsByClassName('chatUser');
--- backend/static/chat.js
+++ backend/static/chat.js
@@ -1,21 +1,40 @@
-
-for (let i = 0; i < chatIaName.length; i++) {
-    chatIaName[i].innerText = `${assistantSelected}:`;
+/* Este era el ideal pero al recargase la pagina da error al intentar reDeclarar varibles existentes
+let userName = sessionStorage.getItem('userName'); `${userName}:`;
+let chatUserName = document.getElementsByClassName('chatUser');
+*/
+// Mostrar el nombre del usuario
+for (let i = 0; i < (document.getElementsByClassName('chatUser')).length; i++) {
+    document.getElementsByClassName('chatUser')[i].innerText = `${sessionStorage.getItem('userName')} :`;
 }
 
-/* for (let i = 0; i < chatIaContainer.length; i++) {
-    chatIaContainer[i].appendChild = `<img src="../static/${assistantSelectedId}.jfif" alt="${assistantSelectedId}" class="assistant-icon" >`;
-} */
-for (let i = 0; i < chatIaContainer.length; i++) {
+/* let assistantSelected = sessionStorage.getItem('assistantSelected');
+let chatIaName = document.getElementsByClassName('chatIa');
+ */
+// Mostrar el nombre del asistente
+for (let i = 0; i < (document.getElementsByClassName('chatIa')).length; i++) {
+    (document.getElementsByClassName('chatIa'))[i].innerText = `${sessionStorage.getItem('assistantSelected')} :`;
+}
+
+/* let assistantSelectedId = sessionStorage.getItem('assistantSelectedId');
+let chatIaContainer = document.getElementsByClassName('chat-ia-container');
+ */
+for (let i = 0; i < (document.getElementsByClassName('chat-ia-container')).length; i++) {
     // Crear un elemento <img> y establecer sus atributos
     let imgElement = document.createElement('img');
-    imgElement.src = `../static/${assistantSelectedId}.jfif`;
-    imgElement.alt = assistantSelectedId;
+    imgElement.src = `../static/${sessionStorage.getItem('assistantSelectedId')}.jfif`;
+    imgElement.alt = sessionStorage.getItem('assistantSelectedId');
     imgElement.className = 'assistant-icon';
 
     // Agregar el elemento <img> al contenedor sin reemplazar el contenido existente
-    chatIaContainer[i].appendChild(imgElement);
+    document.getElementsByClassName('chat-ia-container')[i].appendChild(imgElement);
 }
-for (let i = 0; i < chatUserName.length; i++) {
-    chatUserName[i].innerText = `${userName}:`;
-}
+
+document.getElementById("salir-btn").addEventListener("click", () => {
+    sessionStorage.removeItem("assistantSelected");
+    sessionStorage.removeItem("assistantSelectedId");
+    /*     assistantSelected = "";
+        assistantSelectedId = "";
+        chatIaContainer = "";
+        chatUserName = "";
+        userName = ""; */
+})
